f0bc91c34ae7e0d60b9cac9bb03b1643e5fc1c19
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-06-20 20:31:21 +1000

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+**Jekyll i18n** is a plugin that enables simplistic multi-language site designs using Jekyll. It is written by [Liam Z](liamz.co) and licensed under the GPLv3.
+
+This is currently what needs to be implemented:
+* Localised content
+* Automatic tagging of posts according to langauge
+
+# Tutorial
+The basic principle is that we distinguish between three types of content:
+1. **translated** — content translated fully for a single language.
+2. **localised** — generic template that has phrases localised
+3. **non-content** — non-translatable or binary data
+
+We identify the type by part of the filename. 
+
+### Translated content
+Translated content is that which has a language code directly following its title but preceding its file extension — e.g. **POST-NAME.LANG.EXT**. These files will be generated and **written to a subdirectory named after the language** — e.g. /LANG/POST-NAME.
+
+This plugin injects itself at the last stage of permalinks, so your permalinks will remain intact. 
+
+### Localised content
+Localised content is identified differently from translated content: instead of a language code, localised content has the identifier 'mul' (which is a special language identifier for content of multiple languages).
+
+Although not restricted to localised content, a filter is made available specifically for the purposes of translating phrases — `t`. Translation files are stored in **\_i18n/LANG.yml** and contains a mapping of keys to translations for a specific language. For example, a file fr.yml for French translations:
+```
+fr:
+  Quote of the day: "Le citation du jour"
+  interesting: intéressant
+```
+
+In a localised file, friend.mul.md for example:
+```
+<h3>{%t Quote of the day %}. <small>{%t interesting %}!</small></h3>
+```
+
+The following would be written to /fr/friend.html:
+```
+<h3>Le citation du jour. <small>intéressant!</small></h3>
+```
+
+Localised content will be generated for every language that has translations in **\_i18n**.
+
+### Variables
+This plugin makes the `page.lang` variable available, which is useful in customising layouts. 
---
+++ jekyll-i18n.rb
@@ -0,0 +1,117 @@
+# jekyll-i18n
+# — A simple plugin for multilingual Jekyll sites
+# Author: Liam Edwards-Playne <liamz.co>
+# License: GPLv3
+
+require 'i18n'
+require 'pathname'
+
+module Jekyll
+	I18n.load_path += Dir['_i18n/*.yml']
+	
+	class TranslateTag < Liquid::Tag
+		
+		def initialize(tag_name, text, tokens)
+			super
+			@text = text
+		end
+		
+		# Gets a translation of a key according to the page.lang
+		def render(context)
+			# See https://www.iana.org/assignments/language-subtag-registry for full list of tags
+			I18n.locale = context.registers[:page]['lang'].intern
+			I18n.t(@text.strip)
+		end
+		
+	end
+	
+	module Convertible
+		alias_method :_read_yaml, :read_yaml
+		
+		# Enhances the original method to extract the language from the extension.
+		def read_yaml(base, name)
+			rv = _read_yaml(base, name)
+			
+			# Infer language from first dot in filename.
+			lang = (name.split '.')[1]
+			
+			if lang == 'mul' or I18n.available_locales.include? lang.intern
+				data['lang'] = lang
+			else
+				# Default language is undefined.
+				# If we are procesing a layout, we don't set a language/
+				# This is so when we do render_liquid, we don't override the page's lang.
+				data['lang'] = 'und' if not self.is_a? Jekyll::Layout
+			end
+			
+			rv
+		end
+		
+		alias_method :_render_liquid, :render_liquid
+		
+		def render_liquid(content, payload, info)
+			info[:registers][:page]['lang'] = data['lang']
+			_render_liquid(content, payload, info)
+		end
+		
+	end
+	
+	class Page
+		
+		alias_method :_url, :url
+		# Enhances original by applying /LANG/URL.
+		def url			
+			@url = _url
+			lang = data['lang']
+			# For 'mul' we generate multiple pages — inappropriate to have a url here.
+			# For 'und' there is no language — return.
+			if lang == 'mul' or lang == 'und'
+				return @url
+			end
+			
+			# Gets filename
+			name = Pathname.new(@url).basename.to_s
+			# this could be bad if the directory has the same name as the file.
+			# XXX substitute the last occurance in a string, not the first.
+			nameWithoutLang = @url.sub(name, name.sub('.'+lang, ''))
+			nameWithoutLang == '/index/' ? "/#{lang}/index.html" : "/#{lang}/"+nameWithoutLang
+		end
+		
+	end
+	
+	# XXX this could probably be made more Ruby-like.
+	class Post
+	
+		alias_method :_url, :url
+		# Enhances original by applying /LANG/URL
+		def url			
+			@url = _url
+			lang = data['lang']
+			# For 'mul' we generate multiple pages — inappropriate to have a url here
+			# For 'und' there is no language — return
+			if lang == 'mul' or lang == 'und'
+				return @url
+			end
+			
+			# Gets filename
+			name = Pathname.new(@url).basename.to_s
+			# this could be bad if the directory has the same name as the file
+			# XXX substitute the last occurance in a string, not the first
+			nameWithoutLang = @url.sub(name, name.sub('.'+lang, ''))
+			nameWithoutLang == '/index/' ? "/#{lang}/index.html" : "/#{lang}"+nameWithoutLang
+		end
+	
+	end
+	
+	class Site
+		
+		alias_method :_aggregate_post_info, :aggregate_post_info
+		def aggregate_post_info(post)
+			self.tags[post.data['lang']] << post
+			_aggregate_post_info(post)
+		end
+		
+	end
+end
+
+Liquid::Template.register_tag('t', Jekyll::TranslateTag)
